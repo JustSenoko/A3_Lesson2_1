d34b990481c31d9669672add7a355bd8d1dc7b10
JustSenoko <just.senoko@gmail.com>
2019-09-18 17:44:17 +0300

    элементы макета

--- app/src/main/java/com/example/lesson2_1/MainActivity.java
+++ app/src/main/java/com/example/lesson2_1/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.lesson2_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
     }
 }
